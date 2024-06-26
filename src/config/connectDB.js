60803d2b4a012b0bbce716d367ef9a689139eb69
hieuChiNguyen <hieu.nc1707bkhn@gmail.com>
2024-06-26 23:50:45 +0700

    config db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,9 +1,21 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config()
+// const sequelize = new Sequelize('medx', 'root', null, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     logging: false
+// });
 
-const sequelize = new Sequelize('medx', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'medx', // Tên cơ sở dữ liệu
+    process.env.DB_USERNAME || 'root', // Tên người dùng
+    process.env.DB_PASSWORD || null,   // Mật khẩu
+    {
+        host: process.env.DB_HOST || 'localhost', // Địa chỉ host
+        port: process.env.DB_PORT || 3306,        // Cổng kết nối
+        dialect: process.env.DB_DATABASE || 'mysql',
+        logging: false
+    }
+);
 
 module.exports = sequelize
